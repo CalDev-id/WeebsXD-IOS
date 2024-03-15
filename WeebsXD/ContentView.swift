3f5221a532d2dc629d19fa0117eb918e25e7e996
Heical Chandra <haical31@gmail.com>
2024-03-15 11:29:54 +0700

    Initial Commit

---
+++ WeebsXD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeebsXD
+//
+//  Created by Heical Chandra on 15/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeebsXD/WeebsXDApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeebsXDApp.swift
+//  WeebsXD
+//
+//  Created by Heical Chandra on 15/03/24.
+//
+
+import SwiftUI
+
+@main
+struct WeebsXDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
